d743e4e4b5c67743b451473cc0035128a211a0ec
Peyton Gilliam <bryant.peyton@yahoo.com>
2022-04-04 11:00:01 -0500

    Adding Files

---
+++ static/js/BellyButton_bubble_chart_starter_code.js
@@ -0,0 +1,34 @@
+// Bar and Bubble charts
+// Create the buildCharts function.
+function buildCharts(sample) {
+  // Use d3.json to load and retrieve the samples.json file 
+  d3.json("samples.json").then((data) => {
+    
+
+    // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("bubble", [bubbleData], bubbleLayout); 
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = {
+      x: otuIDs,
+      y: sampleVals,
+      text: otuLabs,
+      mode: 'markers',
+      marker: {
+        size: sampleVals,
+        color: otuIDs
+      }
+    };
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      xaxis: {title: "OTU ID"},
+      showlegend: false
+    };
+    
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", [bubbleData], bubbleLayout); 
+  });
+}
